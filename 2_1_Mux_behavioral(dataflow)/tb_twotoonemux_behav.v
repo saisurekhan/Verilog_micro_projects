d0f6cc4fa3f6d3de299ff849aebfe748562467f0
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2025-04-28 10:39:44 -0500

    Add files via upload
    Implemented 2:1 multiplexer (MUX) using behavioral modeling (Verilog)

---
+++ 2_1_Mux_behavioral(dataflow)/tb_twotoonemux_behav.v
@@ -0,0 +1,50 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/21/2025 03:21:45 PM
+// Design Name: 
+// Module Name: tb_twotoonemux_behav
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module tb_twotoonemux_behav(
+
+    );
+    
+    reg A,B,S;
+    wire Z;
+    
+    twotoone_mux_behavioral uut(
+    .S(S),
+    .A(A),
+    .B(B),
+    .Z(Z)
+    );
+    
+    initial begin
+    
+    #10;
+    S=0; A=0;
+    #25;
+    S=1; B=1;
+    #25;
+    S=0; A=1;
+    #25;
+    S=1; B=0;
+    #25;
+    end
+    
+endmodule
---
+++ 2_1_Mux_behavioral(dataflow)/twotoone_mux_behavioral.v
@@ -0,0 +1,36 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 04/21/2025 03:16:40 PM
+// Design Name: 
+// Module Name: twotoone_mux_behavioral
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module twotoone_mux_behavioral(
+    input A,
+    input B,
+    input S,
+    output Z
+    );
+    
+    wire X,Y,S_bar;
+    assign S_bar = ~(S);
+    assign X = (A & S_bar);
+    assign Y = (B & S);
+    assign Z = (X | Y);
+    
+endmodule
