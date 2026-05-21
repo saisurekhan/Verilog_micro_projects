d585bae865c3db9b39b1a9f9d0b24d308c349f87
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:22:18 -0500

    Update tb_Decoder3_8_behav.v

--- 3_8_decoder_behavioral/tb_Decoder3_8_behav.v
+++ 3_8_decoder_behavioral/tb_Decoder3_8_behav.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/28/2025 05:01:42 PM
-// Design Name: 
-// Module Name: tb_Decoder3_8_behav
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
