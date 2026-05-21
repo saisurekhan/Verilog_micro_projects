caa8ec6bdbb23d85c9b2da00424626e68b6d4b30
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:24:06 -0500

    Update tb_mux41_behav_alw.v

--- 4_1_Mux_behavioral_using_always/tb_mux41_behav_alw.v
+++ 4_1_Mux_behavioral_using_always/tb_mux41_behav_alw.v
@@ -1,25 +1,7 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 09:25:39 PM
-// Design Name: 
-// Module Name: tb_mux41_behav_alw
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module tb_mux41_behav_alw(
     );
     
