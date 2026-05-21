c297832d8968b8a101a91bb415f56c6c70139d7d
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:20:19 -0500

    Update twotoone_mux_nand_structural.v

--- 2_1_Mux_using_Nand_structural_flow/twotoone_mux_nand_structural.v
+++ 2_1_Mux_using_Nand_structural_flow/twotoone_mux_nand_structural.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 03:47:01 PM
-// Design Name: 
-// Module Name: twotoone_mux_nand_structural
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
