3010c7409feeb6bba11c6e3c53cf4540d0d78882
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:18:42 -0500

    Update tb_twotoone_mux_nand_behavioral.v

--- 2_1_Mux_using_Nand_behavioral_flow/tb_twotoone_mux_nand_behavioral.v
+++ 2_1_Mux_using_Nand_behavioral_flow/tb_twotoone_mux_nand_behavioral.v
@@ -1,21 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 04:06:00 PM
-// Design Name: 
-// Module Name: tb_twotoone_mux_nand_behavioral
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
