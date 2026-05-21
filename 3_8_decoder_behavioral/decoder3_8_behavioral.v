f040d3071952db45b8e456373c0f6a8345a764d2
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:22:10 -0500

    Update decoder3_8_behavioral.v

--- 3_8_decoder_behavioral/decoder3_8_behavioral.v
+++ 3_8_decoder_behavioral/decoder3_8_behavioral.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/28/2025 04:21:33 PM
-// Design Name: 
-// Module Name: decoder3_8_behavioral
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
