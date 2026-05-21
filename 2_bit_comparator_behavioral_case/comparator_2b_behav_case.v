5d553b56e0a19e934d847679a2abbcdccd3e618c
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:21:16 -0500

    Update comparator_2b_behav_case.v

--- 2_bit_comparator_behavioral_case/comparator_2b_behav_case.v
+++ 2_bit_comparator_behavioral_case/comparator_2b_behav_case.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/21/2025 03:24:15 PM
-// Design Name: 
-// Module Name: comparator_2b_behav_case
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
