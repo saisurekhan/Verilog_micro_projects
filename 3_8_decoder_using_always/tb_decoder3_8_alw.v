a4183083c6cb7ba89cc9dc285fd13cb9e54e06d8
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:23:02 -0500

    Update tb_decoder3_8_alw.v

--- 3_8_decoder_using_always/tb_decoder3_8_alw.v
+++ 3_8_decoder_using_always/tb_decoder3_8_alw.v
@@ -1,25 +1,6 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/04/2025 02:15:50 PM
-// Design Name: 
-// Module Name: tb_decoder3_8_alw
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
-
-
 module tb_decoder3_8_alw(
     );
     
