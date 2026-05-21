40a5775cabd24ee226ca2e8dfe8f0be80056adad
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:19:59 -0500

    Update twotoone_mux_nand_behav_using_always.v

--- 2_1_Mux_using_Nand_behavioral_with_always/twotoone_mux_nand_behav_using_always.v
+++ 2_1_Mux_using_Nand_behavioral_with_always/twotoone_mux_nand_behav_using_always.v
@@ -1,21 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 04:11:09 PM
-// Design Name: 
-// Module Name: twotoone_mux_nand_behav_using_always
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
