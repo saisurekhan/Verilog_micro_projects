b89433213e26b127ec2f8b7c748c0171c0d5d2f9
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:18:52 -0500

    Update twotoone_mux_nand_behav.v

--- 2_1_Mux_using_Nand_behavioral_flow/twotoone_mux_nand_behav.v
+++ 2_1_Mux_using_Nand_behavioral_flow/twotoone_mux_nand_behav.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 03:56:25 PM
-// Design Name: 
-// Module Name: twotoone_mux_nand_behav
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module twotoone_mux_nand_behav(
     input S,
