0e72127543b6275bbd7a3897a3fccf59b49a8f62
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:04:53 -0500

    Update twotoone_mux.v

--- 2_1_Multiplexer_structural_flow/twotoone_mux.v
+++ 2_1_Multiplexer_structural_flow/twotoone_mux.v
@@ -1,25 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-//
-// Create Date: 04/21/2025 01:39:44 PM
-// Design Name: 
-// Module Name: twotoone_mux------structural
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module twotoone_mux(
     input wire A,B,S,
@@ -28,7 +9,7 @@ module twotoone_mux(
     
    wire X,Y,S_bar;
    not(S_bar,S);
-   and (X,A,S_bar);
+    and (X,A, S_bar);
    and (Y,B,S);
    or(Z,X,Y);
        
