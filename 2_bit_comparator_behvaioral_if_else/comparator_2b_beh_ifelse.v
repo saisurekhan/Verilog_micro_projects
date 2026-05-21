bea76fbd397dac898c9e79ed3571bd8aa62ae7a9
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:21:33 -0500

    Update comparator_2b_beh_ifelse.v

--- 2_bit_comparator_behvaioral_if_else/comparator_2b_beh_ifelse.v
+++ 2_bit_comparator_behvaioral_if_else/comparator_2b_beh_ifelse.v
@@ -1,23 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/21/2025 02:38:20 PM
-// Design Name: 
-// Module Name: comparator_2b_beh_ifelse
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
