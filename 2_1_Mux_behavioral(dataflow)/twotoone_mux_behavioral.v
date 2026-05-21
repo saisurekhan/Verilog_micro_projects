c81dd26ee995d6dbda258655d9c619cbfba1957a
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:05:20 -0500

    Update twotoone_mux_behavioral.v

--- 2_1_Mux_behavioral(dataflow)/twotoone_mux_behavioral.v
+++ 2_1_Mux_behavioral(dataflow)/twotoone_mux_behavioral.v
@@ -1,25 +1,6 @@
 `timescale 1ns / 1ps
 `default_nettype none
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 03:16:40 PM
-// Design Name: 
-// Module Name: twotoone_mux_behavioral
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module twotoone_mux_behavioral(
     input wire A,
