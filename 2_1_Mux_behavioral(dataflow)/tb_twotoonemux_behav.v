56b685431f4edf51afb0950a57799fb0e6cffe62
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:05:09 -0500

    Update tb_twotoonemux_behav.v

--- 2_1_Mux_behavioral(dataflow)/tb_twotoonemux_behav.v
+++ 2_1_Mux_behavioral(dataflow)/tb_twotoonemux_behav.v
@@ -1,24 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/21/2025 03:21:45 PM
-// Design Name: 
-// Module Name: tb_twotoonemux_behav
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 
 module tb_twotoonemux_behav(
 
