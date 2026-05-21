35ca579ad343a0e37afa4b5e48a563d70cfdd426
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:22:42 -0500

    Update tb_decoder3_8_struct.v

--- 3_8_decoder_structural/tb_decoder3_8_struct.v
+++ 3_8_decoder_structural/tb_decoder3_8_struct.v
@@ -1,21 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/28/2025 04:08:04 PM
-// Design Name: 
-// Module Name: tb_decoder3_8_struct
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
