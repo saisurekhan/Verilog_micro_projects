e158d34724cc333b07edb6d1261bc89b77e7de10
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:22:34 -0500

    Update Decoder3_8_struct.v

--- 3_8_decoder_structural/Decoder3_8_struct.v
+++ 3_8_decoder_structural/Decoder3_8_struct.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 04/28/2025 04:03:45 PM
-// Design Name: 
-// Module Name: Decoder3_8_struct
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
