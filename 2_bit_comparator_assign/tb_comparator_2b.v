cbacb56ca9b70bd5bb5c6b64788ff2dbeff1f165
saisurekhan <44650975+saisurekhan@users.noreply.github.com>
2026-05-21 14:21:06 -0500

    Update tb_comparator_2b.v

--- 2_bit_comparator_assign/tb_comparator_2b.v
+++ 2_bit_comparator_assign/tb_comparator_2b.v
@@ -1,22 +1,5 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 05/21/2025 02:28:41 PM
-// Design Name: 
-// Module Name: tb_comparator_2b
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
 //////////////////////////////////////////////////////////////////////////////////
 
 
